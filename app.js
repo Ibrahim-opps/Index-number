76c173179a2f038a279f3145bc5ad78f082ca1aa
Afaf Afridi <ibrahim.hasan00710@gmail.com>
2025-07-08 14:38:26 +0500

    fruits commit

---
+++ app.js
@@ -0,0 +1,12 @@
+fruits = ['apple', 'banana', 'mango', 'orange',
+    'strawberry']
+    document.write(fruits[0] + '<br>');
+    document.write(fruits[1] + '<br>');
+    document.write(fruits[2] + '<br>');
+    document.write(fruits[3] + '<br>');
+    document.write(fruits[4] + '<br>');
+    document.write("at index 0: " + fruits[0] + '<br>');
+    document.write("at index 1: " + fruits[1] + '<br>');
+    document.write("at index 2: " + fruits[2] + '<br>');
+    document.write("at index 3: " + fruits[3] + '<br>');
+    document.write("at index 4: " + fruits[4] + '<br>');
